eb96008a3afdcfa684b770721b7009c229065747
Jantz <jantzillac.93@gmail.com>
2018-05-05 03:02:00 -0500

    Remove API KEY from project, add network connectivity warning, and create sharedPreferences and star button logic for favorite movies.

--- app/src/main/java/com/creativesourceapps/android/cinemate/DetailActivity.java
+++ app/src/main/java/com/creativesourceapps/android/cinemate/DetailActivity.java
@@ -1,7 +1,10 @@
 package com.creativesourceapps.android.cinemate;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,16 +16,41 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Movie movie = getIntent().getParcelableExtra("parcel_data");
-        ImageView imageView = findViewById(R.id.imageView);
+        final SharedPreferences sharedpreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedpreferences.edit();
+
+        final Movie movie = getIntent().getParcelableExtra("parcel_data");
+        final ImageView imageView = findViewById(R.id.imageView);
+        final ImageView imageView2 = findViewById(R.id.imageView2);
+
+        if(sharedpreferences.getString("Favorite"+movie.id,"false").equals("true"))
+            imageView2.setImageResource(android.R.drawable.btn_star_big_on);
+
         TextView title = findViewById(R.id.textView);
         TextView releaseDate = findViewById(R.id.textView2);
         TextView rating = findViewById(R.id.textView3);
         TextView description = findViewById(R.id.textView4);
+
         releaseDate.setText(movie.releaseDate);
         rating.setText(movie.voteAverage);
         title.setText(movie.title);
         description.setText(movie.overview);
         Picasso.with(getApplicationContext()).load("http://image.tmdb.org/t/p/w185/"+movie.poster).into(imageView);
+
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(sharedpreferences.getString("Favorite"+movie.id,"false").equals("true")) {
+                    imageView2.setImageResource(android.R.drawable.btn_star_big_off);
+                    editor.putString("Favorite"+movie.id, "false");
+                    editor.commit();
+                }
+                else {
+                    imageView2.setImageResource(android.R.drawable.btn_star_big_on);
+                    editor.putString("Favorite"+movie.id, "true");
+                    editor.commit();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/creativesourceapps/android/cinemate/MainActivity.java
+++ app/src/main/java/com/creativesourceapps/android/cinemate/MainActivity.java
@@ -3,6 +3,8 @@ package com.creativesourceapps.android.cinemate;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Parcelable;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -25,6 +27,13 @@ import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
     public MoviesAdapter requestMovieData() {
         String response;
         SharedPreferences sharedpreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
@@ -33,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
                                 //**\\**//**\\YOUR API KEY GOES HERE//**\\**//**\\
 
-                        final String MOVIES_API_KEY = "6e520b25b87418f51e3f5d6be319d4ae";
+                        final String MOVIES_API_KEY = "YOUR API GOES HERE";
 
                                 //**\\**//**\\YOUR API KEY GOES HERE//**\\**//**\\
 
@@ -48,29 +57,36 @@ public class MainActivity extends AppCompatActivity {
 
         HttpRequest getRequest = new HttpRequest();
 
-        try {
-            response = getRequest.execute(myUrl).get();
+        if(isOnline()) {
+            try {
+                response = getRequest.execute(myUrl).get();
 
-            JSONObject json = new JSONObject(response);
-            JSONArray items = json.getJSONArray("results");
+                JSONObject json = new JSONObject(response);
+                JSONArray items = json.getJSONArray("results");
 
-            for (int i = 0; i < items.length(); i++) {
-                JSONObject resultObject = items.getJSONObject(i);
-                Movie movie = new Movie(resultObject.getInt("id"),
-                        resultObject.getString("vote_average") + "/10",
-                        resultObject.getString("title"),
-                        resultObject.getString("poster_path"),
-                        resultObject.getString("overview"),
-                        resultObject.getString("release_date"));
-                movies.add(movie);
+                for (int i = 0; i < items.length(); i++) {
+                    JSONObject resultObject = items.getJSONObject(i);
+                    Movie movie = new Movie(resultObject.getInt("id"),
+                            resultObject.getString("vote_average") + "/10",
+                            resultObject.getString("title"),
+                            resultObject.getString("poster_path"),
+                            resultObject.getString("overview"),
+                            resultObject.getString("release_date"));
+                    movies.add(movie);
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch(NullPointerException e){
+                e.printStackTrace();
+                Toast.makeText(this,"Unable to get movie data. Make sure you have added your \"API KEY\" to MainActivity.java", Toast.LENGTH_LONG).show();
             }
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
+        } else {
+            Toast.makeText(this,"No Network Connection. Please reconnect and reload page.", Toast.LENGTH_LONG).show();
         }
 
         MoviesAdapter moviesAdapter = new MoviesAdapter(this, movies);
@@ -85,7 +101,6 @@ public class MainActivity extends AppCompatActivity {
 
                 Movie item_clicked = movies.get(position);
 
-                Toast.makeText(getApplicationContext(), item_clicked.title, Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
                 intent.putExtra("parcel_data", (Parcelable) item_clicked);
                 startActivity(intent);
